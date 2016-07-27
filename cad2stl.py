73a208d551f7ea968134e8f166d36592177c822a
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-07-27 14:58:57 -0500

    Write to stderr instead of stdout.

--- cad2stl.py
+++ cad2stl.py
@@ -9,7 +9,7 @@ if len(sys.argv) == required_args + 1:
     in_path  = sys.argv[1]
     out_path = sys.argv[2]
 else:
-    print "{} /path/to/input /path/to/output".format(sys.argv[0])
+    sys.stderr.write("Usage: {} /path/to/input /path/to/output\n".format(sys.argv[0]))
     sys.exit(1)
 
 in_part = Part.read(in_path)
