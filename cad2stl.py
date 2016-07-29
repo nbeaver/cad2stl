4a20444a241d5defb3e1534cc1b20bfd83399efe
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-07-29 08:30:51 -0500

    Check that input path exists.

--- cad2stl.py
+++ cad2stl.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python2
 
 import sys
+import os
 
 required_args = 2
 if len(sys.argv) == required_args + 1:
@@ -8,6 +9,10 @@ if len(sys.argv) == required_args + 1:
     out_path = sys.argv[2]
 else:
     sys.stderr.write("Usage: {} /path/to/input /path/to/output\n".format(sys.argv[0]))
+    sys.exit(1)
+
+if not os.path.isfile(in_path):
+    sys.stderr.write("Error: input is not a file: {}\n".format(in_path))
     sys.exit(1)
 
 sys.path.append('/usr/lib/freecad/lib')
