c81e1ebfb7ddcc5aa9a575b465291f40b0e589b2
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-07-29 08:24:00 -0500

    Wait to import the FreeCAD library until after checking arguments.

--- cad2stl.py
+++ cad2stl.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python2
 
 import sys
-sys.path.append('/usr/lib/freecad/lib')
-import FreeCAD, Part
 
 required_args = 2
 if len(sys.argv) == required_args + 1:
@@ -11,6 +9,9 @@ if len(sys.argv) == required_args + 1:
 else:
     sys.stderr.write("Usage: {} /path/to/input /path/to/output\n".format(sys.argv[0]))
     sys.exit(1)
+
+sys.path.append('/usr/lib/freecad/lib')
+import FreeCAD, Part
 
 in_part = Part.read(in_path)
 in_part.exportStl(out_path)
