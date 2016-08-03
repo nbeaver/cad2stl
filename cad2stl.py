6cd178cbf00586672b295529f61113c40a63f9f6
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-08-02 23:44:12 -0500

    Improved error handling.

--- cad2stl.py
+++ cad2stl.py
@@ -1,26 +1,23 @@
 #! /usr/bin/env python2
 
-import sys
-import os
-
 def convert(in_file, out_file, lib_dir = None):
 
+    import sys
+    import os
+
     if not os.path.isfile(in_file):
-        sys.stderr.write("Error: input is not a file: {}\n".format(in_file))
-        sys.exit(1)
+        raise IOError("could not find input file: {}".format(in_file))
 
     if os.path.isabs(out_file):
         out_dir = os.path.dirname(out_file)
         if not os.path.isdir(out_dir):
-            sys.stderr.write("Error: output directory does not exist: {}\n".format(out_dir))
-            sys.exit(1)
+            raise IOError("output directory does not exist: {}".format(out_dir))
 
     if lib_dir:
         if os.path.isdir(lib_dir):
             sys.path.append(lib_dir)
         else:
-            sys.stderr.write("Error: FreeCAD lib directory does not exist: {}\n".format(args.lib))
-            sys.exit(1)
+            raise IOError("FreeCAD lib directory does not exist: {}".format(lib_dir))
 
     try:
         import FreeCAD
@@ -30,18 +27,15 @@ def convert(in_file, out_file, lib_dir = None):
             if os.path.isdir(libpath):
                 sys.path.append(libpath)
             else:
-                sys.stderr.write("Cannot find path to FreeCad.so")
-                sys.exit(1)
+                sys.stderr.write("Cannot find path to FreeCad.so\n")
+                raise
         elif os.name == 'nt':
-                sys.stderr.write("Cannot find path to FreeCad.pyd\n")
-                sys.stderr.write("Add FreeCAD 'lib' folder to PYTHONPATH\n")
-                sys.stderr.write("Example directory: C:\\Program Files\\FreeCAD 0.16\\lib\n")
-                sys.exit(1)
+                sys.stderr.write("Cannot find path to FreeCad.pwd\n")
+                raise
         else:
-            sys.stderr.write("Error: Unknown OS: {}".format(os.name))
-            sys.exit(1)
+            raise NotImplementedError("Unknown OS: {}".format(os.name))
 
-    import FreeCAD
+    import FreeCAD # no harm in re-importing
     import Part
 
     in_part = Part.read(args.in_file)
